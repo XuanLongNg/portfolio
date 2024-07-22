f2adeb185bcb44ceb983383018a527c0c4c50bef
long <ngxuanlong2k2@gmail.com>
2024-07-22 22:41:31 +0700

    refactor/develop: new base

--- src/configs/constant.js
+++
@@ -1,7 +0,0 @@
-export const SectionHome = {
-  HOME: "home",
-  ABOUT: "about",
-  PROJECT: "project",
-  CONTACT: "contact",
-  FOOTER: "footer",
-};
--- src/routes/appConfig.js
+++
@@ -1,5 +0,0 @@
-class AppConfig {
-  static HOME_PAGE = "/";
-  static NOT_FOUND = "/404";
-}
-export default AppConfig;
